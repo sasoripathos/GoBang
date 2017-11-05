94740e2b703507a583915990b9cd9d923ed619ff
Zhili (Jerry) Pan <sasori.pan.jerry@gmail.com>
2017-11-05 14:44:08 -0500

    Update Summarize12.v

--- check/Summarize12.v
+++ check/Summarize12.v
@@ -1,3 +1,21 @@
+// Copyright Zhili (Jerry) Pan, July 2017
+// Distributed under the terms of the GNU General Public License.
+//
+// This file is part of GoBang.
+//
+// GoBang is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// GoBang is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this file.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
  * This module takes in the check answers of 12 different cases in a row/column, if there is at
  * least one check pass (find 5 adjacent chesses with same color) return the color, else return 0.
